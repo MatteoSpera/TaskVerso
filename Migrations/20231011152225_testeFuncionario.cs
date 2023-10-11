a8d90a63a2dc444f3b5a11454014ef912a9844b3
MatteoSpera <matteo.spera0301@gmail.com>
2023-10-11 12:25:44 -0300

    Classe Funcionario Implementada

---
+++ Migrations/20231011152225_testeFuncionario.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskVerso.Migrations
+{
+    /// <inheritdoc />
+    public partial class testeFuncionario : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Funcionarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Funcionarios", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Funcionarios");
+        }
+    }
+}
---
+++ Models/Funcionario.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TaskVerso.Models
+{
+	[Table("Funcionarios")]
+	public class Funcionario
+	{
+		[Key]
+		public int Id { get; set; }
+
+		[Required]
+		[StringLength(30)]
+		public string Nome { get; set; }
+
+	}
+}

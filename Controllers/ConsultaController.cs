61e50d6fd9a7176196c9cb54dbf36d50e14dc43e
MatteoSpera <matteo.spera0301@gmail.com>
2023-11-22 10:48:20 -0300

    Adicionada Consulta com filtro

---
+++ Controllers/ConsultaController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TaskVerso.Models;
+
+namespace TaskVerso.Controllers
+{
+	public class ConsultaController : Controller
+	{
+		private readonly Contexto contexto;
+		public ConsultaController(Contexto contexto)
+		{
+			this.contexto = contexto;
+		}
+
+		public IActionResult Filtrar()
+		{
+			return View();
+		}
+
+		public IActionResult TarefaFuncionario(string busca)
+		{
+			var trf = contexto.Tarefas
+				.Include(tarefa => tarefa.Categoria)
+				.Include(tarefa => tarefa.Prioridade)
+				.Include(tarefa => tarefa.Funcionario)
+				.Where(tarefa => tarefa.Funcionario.Nome.Contains(busca)) //mostra apenas tarefas que estejam atribuidas ao funcionario de id 3
+				.ToList();
+			return View(trf);
+		}
+	}
+}

ce4ce91d21b93fc22c871a23eefabe75bfc349a0
MatteoSpera <matteo.spera0301@gmail.com>
2023-11-22 20:04:34 -0300

    Adicionada Migração final e configuração em appsettings alterada

---
+++ Migrations/20231122225305_MigrFinal.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskVerso.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigrFinal : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Descricao",
+                table: "Tarefas",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Descricao",
+                table: "Tarefas",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+        }
+    }
+}
